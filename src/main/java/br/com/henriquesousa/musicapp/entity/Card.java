9aae80af65818e551be0f8c8e4b53d20110d478d
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-10-06 13:44:57 -0300

    add card entity

---
+++ src/main/java/br/com/henriquesousa/musicapp/entity/Card.java
@@ -0,0 +1,28 @@
+package br.com.henriquesousa.musicapp.entity;
+
+public class Card {
+
+    private long id;
+    private String question;
+    private String answer;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}

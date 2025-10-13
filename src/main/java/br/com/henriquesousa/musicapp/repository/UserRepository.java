460ac5cbd257268061b37021ae11aa1a5f1706d2
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-10-13 15:52:32 -0300

    list -> collection

--- src/main/java/br/com/henriquesousa/musicapp/repository/UserRepository.java
+++ src/main/java/br/com/henriquesousa/musicapp/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package br.com.henriquesousa.musicapp.repository;
 
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +9,5 @@ import br.com.henriquesousa.musicapp.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "select * from \"user\" where user_name = :userName", nativeQuery = true)
-    // TODO: List -> Collection?
-    List<User> findByUserName(String userName);
+    Collection<User> findByUserName(String userName);
 }

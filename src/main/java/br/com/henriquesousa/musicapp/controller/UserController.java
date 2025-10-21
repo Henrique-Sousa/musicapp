23e0ea3df16afcb0ba3b7aac9f2c19a589dd9aee
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-10-21 20:50:02 -0300

    organize imports

--- src/main/java/br/com/henriquesousa/musicapp/controller/UserController.java
+++ src/main/java/br/com/henriquesousa/musicapp/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.henriquesousa.musicapp.dto.UserResponseDTO;
 import br.com.henriquesousa.musicapp.dto.UserDTO;
+import br.com.henriquesousa.musicapp.dto.UserResponseDTO;
 import br.com.henriquesousa.musicapp.entity.User;
 import br.com.henriquesousa.musicapp.service.UserService;
 

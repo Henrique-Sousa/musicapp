9b0a9c2c1c73f1861b642cdea8380210dd733359
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-11-13 11:34:17 -0300

    organize imports - remove jsonignore annotation

--- src/main/java/br/com/henriquesousa/musicapp/entity/Card.java
+++ src/main/java/br/com/henriquesousa/musicapp/entity/Card.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;

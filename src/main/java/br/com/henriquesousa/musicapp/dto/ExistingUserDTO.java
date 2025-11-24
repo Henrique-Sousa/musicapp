95c8ab8021c36533a08caa7eabc430b72027876e
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-11-24 14:11:49 -0300

    refactor: existing user inherits from new user

--- src/main/java/br/com/henriquesousa/musicapp/dto/ExistingUserDTO.java
+++ src/main/java/br/com/henriquesousa/musicapp/dto/ExistingUserDTO.java
@@ -4,22 +4,22 @@ import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data // cria getters e setters
+@EqualsAndHashCode(callSuper=false)
 @Schema(title = "ExistingUser", name="Existing", description = "existing user dto")
-public class ExistingUserDTO {
+public class ExistingUserDTO extends NewUserDTO {
     @Schema(description = "the UUID of the user", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
     @NotBlank(message = "uuid-required")
     private UUID uuid;
-    @Schema(description = "the name of the user", example = "John Smith")
-    @NotBlank(message = "name-required")
-    private String name;
-    @Schema(description = "the username of the user", example = "john_smith")
-    @NotBlank(message = "username-required")
-    private String userName;
+
+    public ExistingUserDTO(UUID uuid, String name, String userName) {
+        super(name, userName); // Call the parent's constructor
+        this.uuid = uuid;
+    }
 }
+

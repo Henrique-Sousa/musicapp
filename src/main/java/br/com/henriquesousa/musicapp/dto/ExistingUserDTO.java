05ebc5ca51ab3017c17a25892436be1224c7dfaa
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-11-25 10:16:49 -0300

    fix: uuid not blank -> not null

--- src/main/java/br/com/henriquesousa/musicapp/dto/ExistingUserDTO.java
+++ src/main/java/br/com/henriquesousa/musicapp/dto/ExistingUserDTO.java
@@ -3,7 +3,7 @@ package br.com.henriquesousa.musicapp.dto;
 import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -14,12 +14,11 @@ import lombok.NoArgsConstructor;
 @Schema(title = "ExistingUser", name="Existing", description = "existing user dto")
 public class ExistingUserDTO extends NewUserDTO {
     @Schema(description = "the UUID of the user", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
-    @NotBlank(message = "uuid-required")
+    @NotNull(message = "uuid-required")
     private UUID uuid;
 
     public ExistingUserDTO(UUID uuid, String name, String userName) {
-        super(name, userName); // Call the parent's constructor
+        super(name, userName);
         this.uuid = uuid;
     }
 }
-

38b141b82b355b6becb4cb633d8b374ff569d950
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-11-25 10:22:37 -0300

    feat: set some fields as not updatable

--- src/main/java/br/com/henriquesousa/musicapp/entity/User.java
+++ src/main/java/br/com/henriquesousa/musicapp/entity/User.java
@@ -25,16 +25,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, updatable = false)
     private UUID uuid;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(name = "user_name", nullable = false, unique = true)
+    @Column(name = "user_name", nullable = false, unique = true, updatable = false)
     private String userName;
 
-    @Column
+    @Column(nullable = false, updatable = false)
     private Timestamp createdAt;
 
     // new é para nunca ser null

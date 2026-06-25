bdf07bf2472767e08903f1af73ba353d1947fa97
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-06-24 22:02:10 -0300

    feat: make the cards more responsive

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,35 +1,35 @@
-import {Row, Col, Card} from 'antd';
+import {Row, Col, Card, Flex} from 'antd';
 
 import './App.css'
 
 function App() {
   return (
-    <>
-      <Row gutter={[16, 16]}>
-        <Col span={6}>
+    <Flex className="home" justify="center" align="center" style={{height: '100vh'}} vertical>
+      <Row gutter={16} style={{marginBottom: 16}} justify="center">
+        <Col>
           <Card title="Theory">
             Image here
           </Card>
         </Col>
-        <Col span={6}>
-          <Card title="Eart training">
+        <Col>
+          <Card title="Ear training">
             Image here
           </Card>
         </Col>
       </Row>
-      <Row gutter={[16, 16]}>
-        <Col span={6}>
+      <Row gutter={16} justify="center">
+        <Col>
           <Card title="Score reading">
             Image here
           </Card>
         </Col>
-        <Col span={6}>
+        <Col>
           <Card title="Piano">
             Image here
           </Card>
         </Col>
       </Row>
-    </>
+    </Flex>
   )
 }
 

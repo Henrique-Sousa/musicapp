b75386abeca698678fbf8db5291ed6988068040a
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-06-30 23:02:48 -0300

    refactor: use react-router, creat a page for Home

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,40 +1,15 @@
-import {Row, Col, Card, Flex} from 'antd';
-import { GiMusicalScore } from "react-icons/gi";
-import { PiSpeakerHighFill } from "react-icons/pi";
-import { TbPiano } from "react-icons/tb";
-import { LuNotebookPen } from "react-icons/lu";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.less'
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Flex className="home" justify="center" align="center" style={{minHeight: '100vh'}} vertical>
-      <Row gutter={[16, 16]} style={{marginBottom: 16}} justify="center">
-        <Col>
-          <Card title="Theory">
-            <LuNotebookPen size="6rem"/>
-          </Card>
-        </Col>
-        <Col>
-          <Card title="Ear Training">
-            <PiSpeakerHighFill size="6rem" />
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={[16, 16]} justify="center">
-        <Col>
-          <Card title="Score Reading">
-            <GiMusicalScore size="6rem"/>
-          </Card>
-        </Col>
-        <Col>
-          <Card title="Keyboard">
-            <TbPiano size="6rem" />
-          </Card>
-        </Col>
-      </Row>
-    </Flex>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ website/src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import { Row, Col, Card, Flex } from "antd";
+import { GiMusicalScore } from "react-icons/gi";
+import { PiSpeakerHighFill } from "react-icons/pi";
+import { TbPiano } from "react-icons/tb";
+import { LuNotebookPen } from "react-icons/lu";
+
+import "./index.less";
+
+function Home() {
+  return (
+    <Flex className="home" justify="center" align="center" style={{minHeight: '100vh'}} vertical>
+      <Row gutter={[16, 16]} style={{marginBottom: 16}} justify="center">
+        <Col>
+          <Card title="Theory">
+            <LuNotebookPen size="6rem"/>
+          </Card>
+        </Col>
+        <Col>
+          <Card title="Ear Training">
+            <PiSpeakerHighFill size="6rem" />
+          </Card>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} justify="center">
+        <Col>
+          <Card title="Score Reading">
+            <GiMusicalScore size="6rem"/>
+          </Card>
+        </Col>
+        <Col>
+          <Card title="Keyboard">
+            <TbPiano size="6rem" />
+          </Card>
+        </Col>
+      </Row>
+    </Flex>
+  );
+}
+
+export default Home;
